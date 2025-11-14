a46d753230e3054aaeb5713c1bdb0160fbbad35d
School-911 <nguyendangtruong2004thd@gmail.com>
2025-11-14 20:05:41 +0700

    Delete src directory

--- src/layouts/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer style={{ textAlign: "center", padding: "10px", marginTop: "30px" }}>
-      <small>© 2025 Leopard Team</small>
-    </footer>
-  );
-}
